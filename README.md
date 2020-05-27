a4046e351bb099d7a63781a1ab9e94eb297f31da
Nand Kishor <nandkk07@gmail.com>
2020-05-27 12:38:33 +0530

    check

---
+++ README.md
@@ -0,0 +1,102 @@
+# Developer Student Clubs Directory
+
+<img src="https://raw.githubusercontent.com/dsc-x/about/master/images/project.png" width="100%">
+Version: 1.0
+
+[Click Here](https://dsc-x.github.io/)
+
+### :star: the repo to support the project
+
+### If you are a DSC Lead and want your chapter listed here, or want to update the information about your chapter, create an [Issue](https://github.com/dsc-x/dsc-x.github.io/issues). Then, create a PR closing that issue with the updated details for your chapter!
+
+## Overview
+
+The DSC Directory project tries to present information about all DSCs at a common place to improve finding and reach-out opportunities for students in and near the chapter campus.
+
+## Features
+| Feature | Description |
+|---|---|
+| **Fast and optimized** | PWA on Lighthouse |
+| **Works offline** | Can work offline |
+| **Mobile first** | Mobile-friendly web app can be installed as a native app on phones |
+
+## Technology Stack
+
+* [ReactJS](https://reactjs.org/)
+* [Material Design](https://material.io/)
+
+## Contributing
+
+Contributions are always welcome.
+
+### Setup the repo locally
+
+- Clone the repo
+
+```
+git clone https://github.com/dsc-x/dsc-x.github.io.git
+
+cd dsc-x.github.io
+```
+
+- Install npm packages
+
+```
+npm install
+```
+
+- Run locally
+
+```
+npm start
+```
+
+### Using GitHub Issues
+
+- Feel free to use GitHub issues for questions, bug reports, and feature requests
+- Use the search feature to check for an existing issue
+- Include as much information as possible and provide any relevant resources (Eg. screenshots)
+- For bug reports ensure you have a reproducible test case
+ - A pull request with a breaking test would be super preferable here but isn't required
+
+### Submitting a Pull Request
+
+- Squash commits
+- Include relevant test updates/additions
+- Pull requests _must_ be made against `dev` branch. Any other branch (unless specified by the maintainers) will get rejected.
+
+## Contributors
+
+<!-- ALL-CONTRIBUTORS-BADGE:START - Do not remove or modify this section -->
+[![All Contributors](https://img.shields.io/badge/all_contributors-3-orange.svg?style=flat-square)](#contributors-)
+<!-- ALL-CONTRIBUTORS-BADGE:END -->
+
+<b>Maintainer:</b> [Anubhav Singh](https://github.com/xprilion) <br>
+<b>Developers:</b> [Anubhav Singh](https://github.com/xprilion) <br>
+<b>UI Mockup</b> [Samarth Nayyar](https://github.com/samarthdesigns)
+
+<!-- ALL-CONTRIBUTORS-LIST:START - Do not remove or modify this section -->
+<!-- prettier-ignore-start -->
+<!-- markdownlint-disable -->
+<table>
+  <tr>
+    <td align="center"><a href="https://xprilion.com"><img src="https://avatars1.githubusercontent.com/u/7047208?v=4" width="100px;" alt=""/><br /><sub><b>Anubhav Singh</b></sub></a><br /><a href="#projectManagement-xprilion" title="Project Management">📆</a> <a href="#ideas-xprilion" title="Ideas, Planning, & Feedback">🤔</a> <a href="https://github.com/dsc-x/dsc-x.github.io/commits?author=xprilion" title="Code">💻</a> <a href="#maintenance-xprilion" title="Maintenance">🚧</a></td>
+    <td align="center"><a href="http://samrthnayyar.me"><img src="https://avatars0.githubusercontent.com/u/25741135?v=4" width="100px;" alt=""/><br /><sub><b>Samarth Nayyar</b></sub></a><br /><a href="#design-samarthdesigns" title="Design">🎨</a></td>
+    <td align="center"><a href="https://sourcerer.io/ashfaqnisar"><img src="https://avatars0.githubusercontent.com/u/20638539?v=4" width="100px;" alt=""/><br /><sub><b>Ashfaq Nisar</b></sub></a><br /><a href="https://github.com/dsc-x/dsc-x.github.io/commits?author=ashfaqnisar" title="Code">💻</a></td>
+  </tr>
+</table>
+
+<!-- markdownlint-enable -->
+<!-- prettier-ignore-end -->
+<!-- ALL-CONTRIBUTORS-LIST:END -->
+
+This project follows the [all-contributors](https://github.com/all-contributors/all-contributors) specification. Contributions of any kind welcome
+
+## Facing Any Problem or need any Help?
+
+Write us in [issues](https://github.com/issues) section. Our team will try solve your issue within 10-12 hours.<br>
+
+Project is published under the [MIT license](/LICENSE.md).  
+Feel free to clone and modify repo as you want, but don't forget to add reference to authors :)
+
+This project was bootstrapped with [Create React App](https://github.com/facebook/create-react-app).
---
+++ src/App.js
@@ -0,0 +1,176 @@
+import React, { Component } from 'react';
+import MiniChapter from './MiniChapter';
+import FullChapter from './FullChapter';
+import dsclogo from './assets/images/dsclogo.png';
+import './App.sass';
+import data from "./data/fulllist.json";
+
+import SearchBar from 'material-ui-search-bar'
+import { withStyles } from '@material-ui/core/styles';
+import { Hidden, Container, CssBaseline, Grid, Paper } from '@material-ui/core';
+import Drawer from '@material-ui/core/Drawer';
+
+const styles = (theme) => ({
+  root: {
+    flexGrow: 1,
+    width: "100vw",
+    overflowX: "hidden"
+  },
+  paper: {
+    padding: theme.spacing(2),
+    textAlign: 'center',
+    color: theme.palette.text.secondary,
+  },
+  footer: {
+    width: "100%",
+    backgroundColor: theme.palette.background.paper,
+    marginTop: theme.spacing(8),
+    padding: `${theme.spacing(6)}px 0`,
+  }
+});
+
+class App extends Component {
+  constructor(props) {
+    super(props)
+
+    this.state = {
+      searchKey: "",
+      gridSize: 12,
+      cardSize: 3,
+      infoCardOpen: false,
+      gridSpacing: 6,
+      focusCardData: [],
+      searchState: [],
+      renderData: []
+    }
+  }
+
+  initSearch = () => {
+    var filtered = [];
+    filtered  =  data.dscs.map(dsc => dsc)
+    this.setState({renderData: filtered})
+  }
+
+  makeSearch = () => {
+    const self = this;
+    var filteredData=[];
+    filteredData = data.dscs.filter((element)  => element.Campus.toLowerCase().includes(self.state.searchKey.toLowerCase()) ||
+          element.Acronym.toLowerCase().includes(self.state.searchKey.toLowerCase()) ||
+          element.City.toLowerCase().includes(self.state.searchKey.toLowerCase()) ||
+          element.LeadName.toLowerCase().includes(self.state.searchKey.toLowerCase()))
+      
+    
+    self.setState({ renderData: filteredData});
+  }
+
+  clearSearch = () => {
+    this.setState({ searchKey: ""});
+    this.initSearch();
+  }
+
+  handleInfoCardOpen = (data) => {
+    this.setState({ focusCardData: data })
+    this.setState({ infoCardOpen: true })
+    this.setState({ cardSize: 4 })
+    this.setState({ gridSize: 8 })
+    this.setState({ gridSpacing: 10 })
+  }
+
+  handleInfoCardClose = () => {
+    this.setState({ infoCardOpen: false })
+    this.setState({ cardSize: 3 })
+    this.setState({ gridSize: 12 })
+    this.setState({ gridSpacing: 6 })
+  }
+
+  componentDidMount(){
+    this.initSearch();
+  }
+
+  render() {
+    const { classes } = this.props;
+    return (
+      <React.Fragment>
+        <CssBaseline />
+          <Grid container>
+            <Grid item lg={this.state.gridSize} xs={12} style={{ padding: "0 auto !important" }}>
+              <Container component="div">
+                <Hidden xsDown>
+                  <Container component="div" maxWidth="sm" id="logoContainer">
+                    <img src={dsclogo} alt="DSC logo" style={{ padding: "0 3rem" }}></img>
+                  </Container>
+                </Hidden>
+                <Hidden smUp>
+                  <Container component="div" maxWidth="xl" id="logoContainer">
+                    <img src={dsclogo} alt="DSC logo"></img>
+                  </Container>
+                </Hidden>
+                <Container component="div" maxWidth="sm">
+                  <SearchBar
+                    value={this.state.searchKey}
+                    onChange={(newValue) => this.setState({ searchKey: newValue })}
+                    onRequestSearch={() => this.makeSearch()}
+                    onCancelSearch={()=> this.clearSearch()}
+                    style={{
+                      margin: '2rem auto',
+                      maxWidth: "100%",
+                      borderRadius: "500px"
+                    }}
+                    placeholder="Search"
+                  />
+                </Container>
+              </Container>
+              <Container maxWidth="lg" component="div" style={{ marginTop: "6rem" }}>
+                <Hidden lgUp>
+                  <Grid container spacing={0}>
+                    {this.state.renderData.map((dsc, i) => (
+                      <MiniChapter cardSize={this.state.cardSize} handleInfoCardOpen={() => this.handleInfoCardOpen(dsc)} data={dsc} key={i} spacing={0} />
+                    ))}
+                  </Grid>
+                </Hidden>
+                <Hidden mdDown>
+                  <Grid container spacing={this.state.gridSpacing}>
+                    {this.state.renderData.map((dsc, i) => (
+                      <MiniChapter cardSize={this.state.cardSize} handleInfoCardOpen={() => this.handleInfoCardOpen(dsc)} data={dsc} key={i} />
+                    ))}
+                  </Grid>
+                </Hidden>
+              </Container>
+              <footer className={classes.footer}>
+              <Paper className={classes.paper} elevation={0}>
+                  Developed with <span role="img" aria-label="heart">❤️</span> by <a href="https://twitter.com/xprilion">@xprilion</a>
+                    <br></br>
+                    <br></br>
+                    Project is published under the <a href="https://github.com/dsc-x/dsc-x.github.io/blob/dev/LICENSE">MIT license</a>. <br /> 
+                    Feel free to <a href="https://github.com/dsc-x/dsc-x.github.io">clone and modify repo</a> as you want, but don't forget to add reference to contributors :)
+              </Paper>
+            </footer>
+            </Grid>
+            {
+              this.state.infoCardOpen &&
+              (
+                <Hidden mdDown>
+                  <Grid item lg={4} xs={12} style={{ padding: "0 auto !important" }}>
+                    <FullChapter data={this.state.focusCardData} handleInfoCardClose={this.handleInfoCardClose} position="fixed" />
+                  </Grid>
+                </Hidden>
+              )
+            }
+            {
+              this.state.infoCardOpen &&
+              (
+                <Hidden lgUp>
+                  <Drawer anchor="bottom" open={this.state.infoCardOpen} onClose={this.handleInfoCardClose} style={{ zIndex: 99999 }}>
+                    <FullChapter data={this.state.focusCardData} handleInfoCardClose={this.handleInfoCardClose} />
+                  </Drawer>
+                </Hidden>
+              )
+            }
+            
+          </Grid>
+      </React.Fragment>
+    );
+  }
+}
+
+export default withStyles(styles)(App);
